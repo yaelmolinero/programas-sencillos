7d632dcb044972107238abcd893a8f2461b464cb
Yael Molinero <y.molinero20@gmail.com>
2024-10-24 12:12:38 -0600

    Calificacion mas alta

--- src/ProgramasSencillos/ev3/Calificaciones.java
+++ src/ProgramasSencillos/ev3/Calificaciones.java
@@ -14,6 +14,7 @@ public class Calificaciones {
     String[] materias = new String[MATERIAS];
     float[] calificaciones = new float[MATERIAS];
 
+    float calfMayor = 0;
     for (int i = 0; i < MATERIAS; i++) {
       System.out.print("Materia " + (i+1) + ": ");
       materias[i] = scanner.nextLine();
@@ -24,6 +25,8 @@ public class Calificaciones {
       scanner.nextLine();
       calificaciones[i] = calf;
       promedio += calf;
+      if (calf > calfMayor)
+        calfMayor = calf;
     }
     scanner.close();
 
@@ -31,11 +34,12 @@ public class Calificaciones {
     System.out.println("Boleta del alumno: " + alumno);
     System.out.println(PicoColors.magenta("Calificaciones") + "\t" + PicoColors.yellow("Materias"));
     for (int i = 0; i < MATERIAS; i++) {
-      System.out.println(PicoColors.magenta(Float.toString(calificaciones[i])) + "\t" + PicoColors.yellow(materias[i].toString()));
+      System.out.println(PicoColors.magenta(Float.toString(calificaciones[i])) + "\t" + PicoColors.yellow(materias[i]));
     }
 
     promedio = (float) promedio / MATERIAS;
     System.out.println(PicoColors.cyan("El promedio del alumno es: " + promedio));
+    System.out.println(PicoColors.blue("La calificacion más alta es de: " + calfMayor));
     System.out.println("--------------------");
     if (promedio > 6) 
       System.out.println(PicoColors.green("El alumno aprobo!"));
